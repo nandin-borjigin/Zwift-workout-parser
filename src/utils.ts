6ad6d38c4ba760124822f20d9271c3ad1ea74975
Nandiin <nandiin@163.com>
2021-07-26 19:29:21 +0800

    fix: typo

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export function makeError(failedString: string) {
-  return new Error(`Whoop! Your idiot, yet cute, boy friend must have missed something in his code. Call him!
+  return new Error(`Whoops! Your idiot, yet cute, boy friend must have missed something in his code. Call him!
 
 Cannot parse ${failedString}.
 `);
